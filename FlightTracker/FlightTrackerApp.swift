87c878be2dfe8f5eaa3dd6b85750db41cb78c5db
dabdelwahed <dabdelwahed@mobelite.fr>
2022-12-07 15:30:35 +0100

    Initial Commit

---
+++ FlightTracker/FlightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTrackerApp.swift
+//  FlightTracker
+//
+//  Created by Dorra Ben Abdelwahed on 7/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
